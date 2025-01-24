1f0a1c6f141b60c7852bdb9d9c8aff3500aac18d
USER <mylesdavyz@gmail.com>
2025-01-24 20:47:08 +0300

    Finishing up my tech stack

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { About, Footer, Header, Work, Skills } from './container'
+import { About, Footer, Header, Work, Skills, Tech, Contact } from './container'
 import { Navbar } from './components'
 import './App.scss'
 
@@ -11,7 +11,9 @@ const App = () => {
       <Header />
       <About/>
       <Skills />
+      <Tech />
       <Work />
+      <Contact /> 
       <Footer />       
     </div>
   )
--- src/container/index.js
+++ src/container/index.js
@@ -3,3 +3,5 @@ export { default as Footer } from './Footer/Footer';
 export { default as Header } from './Header/Header';
 export { default as Work } from './Work/Work';
 export { default as Skills} from './Skills/Skills';
+export { default as Tech} from './Tech/Tech';
+export { default as Contact} from './Contact/Contact';
